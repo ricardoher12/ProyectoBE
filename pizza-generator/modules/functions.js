ce6f571f4c5ff921a020319ba6bdc80143c8ac9a
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2018-10-04 23:31:14 -0600

    se suben cambios en metodos y evaluacion de errores

--- pizza-generator/modules/functions.js
+++ pizza-generator/modules/functions.js
@@ -1,3 +1,5 @@
+//import { isUndefined } from "util";
+
 class Pizza {
     constructor (id, nombre, forma,  size, ingredientes,  orilla)
     {
@@ -10,49 +12,35 @@ class Pizza {
     }
 
 }
-
-//import { isUndefined } from "util";
 var pizzas = new Map();
-pizzas.set("11", new Pizza('11', 'Mr. Nice', 'Redonda', 'Grande', 'Salsa, queso, tomate, cebolla',  "Si" ));
-pizzas.set('12', new Pizza( '12',  'Narco',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla', "Si" ))
-pizzas.set("13", new Pizza('13',  'Bombasto',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla', "Si" ))
-pizzas.set("14",new Pizza('14',  'Celeritas',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
-pizzas.set("15", new Pizza('15',  'Magneta',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
-pizzas.set("16",new Pizza('16',  'RubberMan',  'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
-pizzas.set("17",new Pizza('17',  'Dynama',  'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
-pizzas.set("18",new Pizza( '18',  'Dr IQ',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
 
-/*module.exports = {
-    Get : function(){
-       return this.Get();
-    },
-    GetId : function(id){
-        return this.Get(id);
-    },
-    Post : function(item){
-        return this.Post(item);
-    },
-    Put :  function(item){
-       return this.Put(item);
-    },
-    Delete: function(id){
-        return this.Delete(id);
-    }
 
-  };*/
-
-exports.Get = function()
-{
-    //return JSON.stringify(pizzas);
-    return "hola";
+exports.CargarData = function(){
+    pizzas.set("11", new Pizza('11', 'Mr. Nice', 'Redonda', 'Grande', 'Salsa, queso, tomate, cebolla',  "Si" ));
+    pizzas.set('12', new Pizza( '12',  'Narco',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla', "Si" ))
+    pizzas.set("13", new Pizza('13',  'Bombasto',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla', "Si" ))
+    pizzas.set("14",new Pizza('14',  'Celeritas',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
+    pizzas.set("15", new Pizza('15',  'Magneta',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
+    pizzas.set("16",new Pizza('16',  'RubberMan',  'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
+    pizzas.set("17",new Pizza('17',  'Dynama',  'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
+    pizzas.set("18",new Pizza( '18',  'Dr IQ',   'Redonda', 'Grande',  'Salsa, queso, tomate, cebolla',  "Si"));
+    
 }
+
+
+
+exports.Get = function(){
+    
+    return JSON.stringify(Array.from(pizzas));
+    //return "hola";
+};
 
 
 
  exports.GetID = function (id){
     try {
         let pizza = pizzas.get(id);
-        if(isUndefined(pizza))
+        if(pizza == null)
         {
             return false;
         }
@@ -64,7 +52,7 @@ exports.Get = function()
         console.log(error);
         return false;
     }
-}
+};
 
 
 
@@ -84,7 +72,7 @@ exports.Delete = function(id){
         console.log(error);
         return false;
     }
-}
+};
 
 exports.Post = function (item){
 try {
@@ -95,7 +83,7 @@ try {
     console.log(error);
     return false;
     }
-}
+};
 
 exports.Put = function (item){
     try{
@@ -110,20 +98,6 @@ exports.Put = function (item){
         console.log(error);
         return false;
     }
-}
+};
 
- exports.Delete = function (id){
-    try {
-        if(isUndefined(pizzas.get(id)))
-        {
-            return false;
-        }else{
-            pizzas.delete(id);
-            return true;
-        }
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
- }
-
+ 
--- pizza-generator/routes/pizza.js
+++ pizza-generator/routes/pizza.js
@@ -3,18 +3,35 @@ var router = express.Router();
 //import {Get} from '../modules/functions';
 var functions = require('../modules/functions.js');
 var GetID = functions.GetID;
+var Post = functions.Post;
+var Put = functions.Put;
+var Delete = functions.Delete;
+var Get = functions.Get;
+functions.CargarData();
+
 
 /* GET pizzas listing. */
 router.get('/', function(req, res, next) {
-  //res.send(functions.Get());
+  
+  res.send(Get());
 });
 
 /* GET pizza with an ID listing */
-/*router.get('/:id', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
   var id = req.params.id;
-  //res.send(functions.Get(id));
+  var pizza = GetID(id);
+
+  if(pizza != false)
+  {
+    res.send(pizza);
+  }else{
+    res.status(404).send('{"message":"Object not found"}');
+  }
+ 
 });
 
+
+/*
 //POST of a pizza item
 app.post('/', function(req, res) {
   var data = req.body.data;
