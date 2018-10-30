24e27ee63f4614a9302896ce7a6f8657c1604a19
ricardoher12 <ricardo.hernandezr2@hotmail.com>
2018-10-29 19:11:30 -0600

    se agrega cors y configuracion docker

--- pizza-generator/app.js
+++ pizza-generator/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var pizzaRouter = require('./routes/pizza');
@@ -18,17 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/api/v1/pizzas', pizzaRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next(createError(404));
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
